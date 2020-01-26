3615d1a293616f6a084ea2e59575f296cfc94746
노주훈 <joohoonroh@naver.com>
2020-01-26 16:58:09 +0900

    배너 슬라이더 기본동작

--- js/common.js
+++ js/common.js
@@ -24,7 +24,40 @@ document.addEventListener('DOMContentLoaded', function(){
   })();
 
   function includeComplete() {
-    console.log('includeComplete');
+    slider();
+  }
+  
+  function slider() {
+    var selectSlider = document.querySelectorAll('.slider');
+    selectSlider.forEach(function (value, index) {
+      var self = selectSlider[index];
+      var self_list = self.querySelector('.slider_list');
+      var self_item = self_list.querySelectorAll('.slider_item');
+      var self_width = self.offsetWidth;
+      self_list.style.left = '0px';
+      self_item.forEach(function (value, index) {
+        var self = self_item[index];
+        self.style.left = index * self_width + 'px';
+      });
+      sliderControl(self, self_list);
+    });
+  }
+  function sliderControl(target, target_list) {
+    var slider_list = target.querySelector('.slider_list');
+    var sliderControl_btn = target.querySelectorAll('.sliderControl .sliderControl_btn');
+    sliderControl_btn.forEach(function (value, index) {
+      var self = sliderControl_btn[index];
+      self.addEventListener('click', function (e) {
+        var nowSliderListLeft = target_list.style.left.split('px')[0];
+        e.target.classList.forEach(function (value, index) {
+          if (value == 'is_prev') {
+            target_list.style.left = Number(nowSliderListLeft) - Number(target_list.offsetWidth) + 'px';
+          } else if (value == 'is_next') {
+            target_list.style.left = Number(nowSliderListLeft) + Number(target_list.offsetWidth) + 'px';
+          }
+        });
+      });
+    });
   }
 
 });
